40c6902a1818fe149496c11c8478137ff3c1457c
KenDesrosiers <kdesrosiers@wpi.edu>
2020-10-26 19:16:26 -0400

    spacing

--- src/App.py
+++ src/App.py
@@ -2,16 +2,13 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 
-#class for main app window, just defines the layout of the app
+
+# class for main app window, just defines the layout of the app
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('mainwindow.ui', self)
         self.show()
-
-
-
-
 
 
 app = QApplication(sys.argv)
