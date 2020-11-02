c477a7d341532b0d913adcb1de97d09de6828ea7
KenDesrosiers <kdesrosiers@wpi.edu>
2020-11-02 17:19:19 -0500

    testing

--- src/App.py
+++ src/App.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
+import datetime
+import time
+from PyQt5.QtCore import QTimer
 
 
 # class for main app window, just defines the layout of the app
@@ -8,8 +11,13 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('mainwindow.ui', self)
+        timer = QTimer(self)
+        timer.timeout.connect(self.displayDateTime)
+        timer.start()
         self.show()
 
+    def displayDateTime(self):
+        self.datetime.setText(datetime.date.today().strftime("%A %b. %d"))# + "    " + time.strftime("%H:%M"))
 
 app = QApplication(sys.argv)
 window = MainWindow()
