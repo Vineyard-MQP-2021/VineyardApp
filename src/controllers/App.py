d41015616b212b1cecab286e8b7057bb63af769e
KenDesrosiers <kdesrosiers@wpi.edu>
2020-11-19 15:42:52 -0500

    refactoring

--- src/controllers/App.py
+++ src/controllers/App.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from src.PageSwitcher import PageSwitcher
+from src.controllers.PageSwitcher import PageSwitcher
 from src.models.APIInfo import APIInfo
 
 app = QApplication(sys.argv)
