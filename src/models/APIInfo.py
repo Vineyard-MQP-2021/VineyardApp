114bb0e2fca0da9920aca0c20ab4b57173893cb2
KenDesrosiers <kdesrosiers@wpi.edu>
2020-11-19 15:48:35 -0500

    better code for APIs

--- src/models/APIInfo.py
+++ src/models/APIInfo.py
@@ -15,32 +15,36 @@ class APIInfo:
             raise Exception("Error")
         else:
             APIInfo.__instance = self
+            self.getipinfo()
+            self.getweatherinfo()
 
-            # In order to get the approximate user's location, their ip address is used
-            # The ipinfo (https://ipinfo.io/) api is used to find approximate latitude and longitude from ip address.
-            ip = socket.gethostbyname(socket.gethostname())
-            ip_url = "http://ipinfo.io/" + ip
-            ip_params = {'token': api_keys.ipAPIKey}
-            ip_api_response = requests.get(url=ip_url, params=ip_params).text
-            try:
-                ip_response_data = json.loads(ip_api_response)
-                self.lat = ip_response_data["loc"].split(",")[0]
-                self.lon = ip_response_data["loc"].split(",")[1]
-                self.tz = ip_response_data["timezone"]
-            except ValueError:
-                print("An error occurred!")
+    def getipinfo(self):
+        # In order to get the approximate user's location, their ip address is used
+        # The ipinfo (https://ipinfo.io/) api is used to find approximate latitude and longitude from ip address.
+        ip = socket.gethostbyname(socket.gethostname())
+        ip_url = "http://ipinfo.io/" + ip
+        ip_params = {'token': api_keys.ipAPIKey}
+        ip_api_response = requests.get(url=ip_url, params=ip_params).text
+        try:
+            ip_response_data = json.loads(ip_api_response)
+            self.lat = ip_response_data["loc"].split(",")[0]
+            self.lon = ip_response_data["loc"].split(",")[1]
+            self.tz = ip_response_data["timezone"]
+        except ValueError:
+            print("An error occurred!")
 
-            # The latitude and longitude from the ipinfo api is used to find the current weather
-            # the openweathermap (http://api.openweathermap.org/data/2.5/weather) api is used
-            weather_url = "http://api.openweathermap.org/data/2.5/weather"
-            weather_params = {'lat': self.lat, 'lon': self.lon, 'units': "imperial", "appid": api_keys.weatherAPIKey}
-            weather_api_response = requests.get(url=weather_url, params=weather_params).text
-            try:
-                weather_response_data = json.loads(weather_api_response)
-                self.weather = weather_response_data["weather"][0]["main"]
-                self.temp = round(weather_response_data["main"]["temp"])
-            except ValueError:
-                print("An error occurred")
+    def getweatherinfo(self):
+        # The latitude and longitude from the ipinfo api is used to find the current weather
+        # the openweathermap (http://api.openweathermap.org/data/2.5/weather) api is used
+        weather_url = "http://api.openweathermap.org/data/2.5/weather"
+        weather_params = {'lat': self.lat, 'lon': self.lon, 'units': "imperial", "appid": api_keys.weatherAPIKey}
+        weather_api_response = requests.get(url=weather_url, params=weather_params).text
+        try:
+            weather_response_data = json.loads(weather_api_response)
+            self.weather = weather_response_data["weather"][0]["main"]
+            self.temp = round(weather_response_data["main"]["temp"])
+        except ValueError:
+            print("An error occurred")
 
     @staticmethod
     def getInstance():
